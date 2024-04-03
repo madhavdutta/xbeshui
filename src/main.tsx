9be25506cd84e0d9a57b2651bfd2946bde4944da
mohdaakib <aakibahmed523@gmail.com>
2024-04-03 20:56:31 +0530

    updated

---
+++ src/main.tsx
@@ -0,0 +1,28 @@
+'use client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "../packages/index.css";
+import { XbeshProvider } from "../packages/core/components/Theme/xBeshTheme/xbeshProvider.tsx";
+import { Toaster } from "../packages/core/components/Feedback/toast/toaster.tsx";
+const RootComponent = () => {
+
+
+  return (
+    <React.StrictMode>
+      <XbeshProvider storageKey={"vite"} defaultTheme={"light"}>
+        <div>
+          <App />
+
+          <Toaster />
+        </div>
+      </XbeshProvider>
+    </React.StrictMode>
+  );
+};
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <>
+    <RootComponent />
+  </>
+);
