73a6eb7acf2518ed7e3ed4fee4101da283fcda53
mohdaakib <aakibahmed523@gmail.com>
2024-04-10 17:45:38 +0530

    fix tab variant

--- packages/core/components/DataDisplay/tabs/tabs.tsx
+++ packages/core/components/DataDisplay/tabs/tabs.tsx
@@ -3,17 +3,26 @@ import * as TabsPrimitive from "@radix-ui/react-tabs";
 
 // import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "../../../../utils";
+import { VariantProps } from "class-variance-authority";
+import { tabsVariants } from "./tabs.config";
 
 const Tabs = TabsPrimitive.Root;
 
 const TabsList = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.List>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
->(({ className, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.List> &
+    VariantProps<typeof tabsVariants>
+>(({ className, variant, ...props }, ref) => (
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-[38px] items-center justify-center rounded-md bg-transparent text-muted-foreground border-b border-gray-300 rounded-none",
+      variant === "outline"
+        ? "inline-flex h-[38px] items-center justify-center rounded-md bg-transparent text-muted-foreground border-b border-gray-300 rounded-none"
+        : variant === "pill"
+        ? "inline-flex h-10 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground"
+        : variant === "underline"
+        ? "inline-flex h-[38px] items-center justify-center rounded-md bg-transparent text-muted-foreground border-b-2 border-gray-300 rounded-none"
+        : "inline-flex h-[38px] items-center justify-center rounded-md bg-transparent text-muted-foreground border-b-2 border-gray-300 rounded-none",
       className
     )}
     {...props}
@@ -23,12 +32,19 @@ TabsList.displayName = TabsPrimitive.List.displayName;
 
 const TabsTrigger = React.forwardRef<
   React.ElementRef<typeof TabsPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
->(({ className, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger> &
+    VariantProps<typeof tabsVariants>
+>(({ className, variant, ...props }, ref) => (
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-2 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-transparent shadow-none rounded-none  data-[state=active]:text-foreground data-[state=active]:border-l data-[state=active]:border-t data-[state=active]:border-r data-[state=active]:border-l-gray-300 data-[state=active]:border-t-gray-300 data-[state=active]:border-r-gray-300 data-[state=active]:border-b-white data-[state=active]:border-b data-[state=active]:rounded-t-sm ",
+      variant === "outline"
+        ? "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-2 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-transparent shadow-none rounded-none  data-[state=active]:text-foreground data-[state=active]:border-l data-[state=active]:border-t data-[state=active]:border-r data-[state=active]:border-l-gray-300 data-[state=active]:border-t-gray-300 data-[state=active]:border-r-gray-300 data-[state=active]:border-b-white data-[state=active]:border-b data-[state=active]:rounded-t-sm"
+        : variant === "pill"
+        ? "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm"
+        : variant === "underline"
+        ? "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-2 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-transparent shadow-none rounded-none  data-[state=active]:text-foreground data-[state=active]:border-b data-[state=active]:border-gray-900 data-[state=active]:border-b-2"
+        : "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-2 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-transparent shadow-none rounded-none  data-[state=active]:text-foreground data-[state=active]:border-b data-[state=active]:border-gray-900 data-[state=active]:border-b-2",
       className
     )}
     {...props}
