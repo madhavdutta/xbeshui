389cda0388d17bdfc8059caee57d951748639d09
spidey-I2R <aakib@idea2reality.io>
2024-05-16 12:57:25 +0530

    added dialog props

--- packages/core/components/Feedback/dialog/dialogType.d.ts
+++ packages/core/components/Feedback/dialog/dialogType.d.ts
@@ -1,22 +1,47 @@
 import type * as DialogPrimitive from "@radix-ui/react-dialog";
 
-type DialogOverlayType = React.ElementRef< DialogPrimitive.Overlay> &
-  React.ComponentPropsWithoutRef< DialogPrimitive.Overlay>;
-type DialogContentType = React.ElementRef< DialogPrimitive.Content> &
-  React.ComponentPropsWithoutRef< DialogPrimitive.Content>;
-type DialogTitleType = React.ElementRef< DialogPrimitive.Title> &
-  React.ComponentPropsWithoutRef< DialogPrimitive.Title>;
-type DialogDescriptionType = React.ElementRef<
-   DialogPrimitive.Description
-> &
-  React.ComponentPropsWithoutRef< DialogPrimitive.Description>;
+export interface dialogVarientConfigType {
+  variants: {
+    size: {
+      default: string;
+      full: string;
+      xl: string;
+      lg: string;
+      md: string;
+      sm: string;
+      xs: string;
+    };
+    radius: {
+      default: string;
+      xs: string;
+      sm: string;
+      md: string;
+      lg: string;
+      xl: string;
+      full: string;
+    };
+  };
+  defaultVariants: {
+    size: keyof dialogVarientConfigType["variants"]["size"];
+    radius: keyof dialogVarientConfigType["variants"]["radius"];
+  };
+}
 
-  type DialogCommonType = React.HTMLAttributes<HTMLDivElement>
+type DialogOverlayType = React.ElementRef<DialogPrimitive.Overlay> &
+  React.ComponentPropsWithoutRef<DialogPrimitive.Overlay>;
+type DialogContentType = React.ElementRef<DialogPrimitive.Content> &
+  React.ComponentPropsWithoutRef<DialogPrimitive.Content>;
+type DialogTitleType = React.ElementRef<DialogPrimitive.Title> &
+  React.ComponentPropsWithoutRef<DialogPrimitive.Title>;
+type DialogDescriptionType = React.ElementRef<DialogPrimitive.Description> &
+  React.ComponentPropsWithoutRef<DialogPrimitive.Description>;
+
+type DialogCommonType = React.HTMLAttributes<HTMLDivElement>;
 
 export type {
   DialogOverlayType,
   DialogContentType,
   DialogTitleType,
   DialogDescriptionType,
-  DialogCommonType
+  DialogCommonType,
 };
